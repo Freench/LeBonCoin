92c0b7f64c3dd492a6795fb9446007919796fa94
Aleaddine El Aji <87655501+AleaddineElAji@users.noreply.github.com>
2021-10-21 16:50:26 +0200

    ObjetSignIn

---
+++ Class/SignIn.php
@@ -0,0 +1,30 @@
+<?php
+    require_once '../Class/Bdd.php';
+    class SignIn extends Bdd {
+        function analyseEntree(){
+            if( isset($_GET['mail']) && isset($_GET['pseudo']) && isset($_GET['passwd']) && (!empty($_GET['mail'])) && (!empty($_GET['pseudo'])) && (!empty($_GET['passwd']))){
+                $mail = strip_tags($_GET['mail']);
+                $pseudo = strip_tags($_GET['pseudo']);
+                $passwd = strip_tags($_GET['passwd']);
+                // ICI on hash le password pour plus de sécurité
+                $passwd = password_hash($passwd, PASSWORD_DEFAULT);
+                return [$mail, $pseudo, $passwd]; 
+            }
+        }
+
+        function insert($table){
+            $sql =  "INSERT INTO utilisateurs (
+                mail_utilisateur,
+                pseudo_utilisateur,
+                mdp_utilisateur) VALUE (?, ?, ?)";
+            var_dump($sql);
+            $pdo = $this->connect();
+            $query = $pdo->prepare($sql);
+            // On injecte (terme scientifique) les valeurs
+            $query -> execute($table);
+
+        }
+    }
+
+
+?>
