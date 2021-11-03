3fd196256fc7339d2d9930336f55b1434729710d
Aleaddine El Aji <87655501+AleaddineElAji@users.noreply.github.com>
2021-11-03 11:34:36 +0100

    CommitPreMerge

--- Php/nav.php
+++ Php/nav.php
@@ -14,8 +14,8 @@
 </nav> -->
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a id="Titre" class="navbar-brand" href="../LeBonCoin/index.php">La Bonne Oca'z</a>
+<nav id="navBarre" class="navbar navbar-expand-lg navbar-light bg-light">
+  <a id="Titre" class="navbar-brand" href="../index.php">La Bonne Oca'z</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
